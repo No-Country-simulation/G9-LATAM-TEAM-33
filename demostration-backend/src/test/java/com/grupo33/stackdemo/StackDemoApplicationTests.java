101576c771d112ca3d22d5370d21b93f2ebd1a7b
Josue Marquez <owaruuu@gmail.com>
2026-07-19 13:29:52 -0400

    add a demostration API to show hwo OCI resources work

---
+++ demostration-backend/src/test/java/com/grupo33/stackdemo/StackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo33.stackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
